d7397f4b4aada8ea005db1e7f3f68702dc3e626e
Siddarood <siddarood005@gmail.com>
2024-03-22 10:33:14 +0530

    Simple Tcp/Ip server created

---
+++ Src/TCP_Server/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TCP_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Server server = new Server();
+                server.StartServer();
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Error occured : ",ex.Message);   
+            }
+        }
+    }
+}
